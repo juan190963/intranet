1d4bed4c2cd2c1775802775bab725ac619a93aa3
Juan Paez <jpaez@pernine.com.co>
2024-06-14 08:19:45 -0500

    feat(login): agrega nuevo login

--- src/components/molecules/LoginForm/LoginForm.tsx
+++ src/components/molecules/LoginForm/LoginForm.tsx
@@ -8,7 +8,9 @@ import usePasswordToggle from '@/utils/hooks/usePasswordToggle';
 import { useAuthStore } from '@/store/auth';
 import { AxiosError } from 'axios';
 import { toast } from 'react-toastify';
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
+import { Logo } from '@/components/atoms/Logo/Logo';
+import useWindowSize from '@/utils/hooks/useWindowSize';
 
 export const LoginForm: React.FC = () => {
 	const {
@@ -19,6 +21,7 @@ export const LoginForm: React.FC = () => {
 	const [showPassword, togglePasswordVisibility] = usePasswordToggle();
 	const { login, user } = useAuthStore();
 	const navigate = useNavigate();
+	const size = useWindowSize();
 
 	useEffect(() => {
 		console.log(user);
@@ -27,7 +30,7 @@ export const LoginForm: React.FC = () => {
 	const onSubmit = async (data: LoginData) => {
 		try {
 			await login(data.email, data.password);
-			navigate("/");
+			navigate('/');
 		} catch (error: unknown) {
 			if (error instanceof AxiosError && error.response) {
 				toast.error(error.response.data.message);
@@ -37,26 +40,24 @@ export const LoginForm: React.FC = () => {
 
 	return (
 		<>
-			<div className='shadow-xl rounded-b-xl'>
-				<div className='flex max-sm:px-5 px-10 items-center justify-center h-32 login__header rounded-t-xl'>
-					<span className='text-white'>
-						!Hola, te damos la bienvenida a la intranet!
-					</span>
+			<div className='flex flex-col justify-center container h-full rounded-b-[50px] sm:rounded-r-[120px]'>
+				<div className='flex justify-center mt-5 sm:mt-0'>
+					<Logo width={size.width < 768 ? '100' : '173'} height='auto' />
 				</div>
-				<div className='flex max-sm:px-5 px-10 items-center justify-center h-20 '>
-					<span className=''>Para acceder, ingresa los siguientes datos:</span>
+				<div className='flex max-sm:px-5 mt-5 mb-2 sm:mt-0 sm:mb-0 px-10 items-center justify-center sm:h-20'>
+					<h1 className='text-white'>BIENVENIDO A NUESTRA INTRANET</h1>
 				</div>
 				<form
 					onSubmit={handleSubmit(onSubmit)}
 					className='pt-2 pb-10 max-sm:px-5 px-10 rounded-b-xl'
 				>
-					<div className='max-w-md block mx-auto'>
+					<div className='max-w-xs block mx-auto'>
 						<div className='mb-2'>
 							<label
 								htmlFor='email'
-								className='block text-base font-medium text-gray-700'
+								className='block text-base font-medium text-white'
 							>
-								Correo electronico registrado
+								email empresarial
 								<input
 									type='email'
 									id='email'
@@ -70,7 +71,7 @@ export const LoginForm: React.FC = () => {
 						<div className='mb-6'>
 							<label
 								htmlFor='password'
-								className='block text-base font-medium text-gray-700'
+								className='block text-base font-medium text-white'
 							>
 								Contraseña
 								<div className='relative'>
@@ -95,17 +96,16 @@ export const LoginForm: React.FC = () => {
 							</label>
 							<ErrorMessage error={errors.password?.message} />
 						</div>
-						<button type='submit' className='w-full mt-5 px-4 py-2 primary-button'>
+						<button
+							type='submit'
+							className='block m-auto mt-5 px-4 py-2 bg-white font-medium rounded-lg'
+						>
 							Iniciar sesión
 						</button>
-						<div className='mt-8 text-center'>
-							{/* <Link
-								to={ROUTES.RESET_PASSWORD}
-								rel='noreferrer'
-								className='text-base'
-							>
-								¿Olvidaste la contraseña?
-							</Link> */}
+						<div className='mt-4 sm:mt-8 text-center'>
+							<Link to='' rel='noreferrer' className='text-white'>
+								Recordar contraseña
+							</Link>
 						</div>
 					</div>
 				</form>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import { LoginForm } from '@/components/molecules/LoginForm/LoginForm';
-import { Slider } from '@/components/molecules/Slider/Slider';
+import imageDesktop from '@/assets/login/images/coverPage.webp';
+import imageMobile from '@/assets/login/images/coverPageMobile.webp';
+import useWindowSize from '@/utils/hooks/useWindowSize';
 
 const Login: React.FC = () => {
-	const images = [
-		'img1',
-		'img2',
-		'img3',
-		'img4',
-		'img5',
-		'img6',
-		'img7',
-		'img8',
-	];
+	const size = useWindowSize();
+	const backgroundImage = size.width < 768 ? imageMobile : imageDesktop;
 
 	return (
-		<div className='flex lg:flex-wrap px-4 max-lg:flex-col p-10 justify-center h-full'>
-			<div className='flex flex-col items-center justify-center max-lg:w-full md:w-2/5 max-lg:hidden'>
-				<Slider images={images} autoPlay showButtons={false} />
-			</div>
-			<div className='md:flex flex-col justify-center w-2/5 max-lg:w-full'>
+		<div className='flex flex-col sm:flex-row gap-y-3 gap-x-8 h-full'>
+			<div className='w-full sm:w-2/5'>
 				<LoginForm />
 			</div>
+			<div
+				className='flex items-center w-full sm:w-3/5 sm:m-5 h-full max-w-full'
+				style={{
+					backgroundImage: `url(${backgroundImage})`,
+					backgroundRepeat: 'no-repeat',
+					backgroundSize: 'cover',
+				}}
+			/>
 		</div>
 	);
 };
 
-export default Login
+export default Login;
